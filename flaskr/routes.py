b1919156332cd4658ae7176a3491d869bdee3feb
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-09-09 15:48:42 +0800

    [#15] added job analysis capability

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template, request
+
+from . import analyzelib
 
 bp = Blueprint("blog", __name__)
 
@@ -7,3 +9,12 @@ bp = Blueprint("blog", __name__)
 def index():
     """Shows the page of the web app."""
     return render_template("index.html")
+
+
+@bp.route("/analyze", methods=["POST"])
+def analyze():
+    """Analyze a given job description."""
+    payload = request.get_json()
+    text = payload["text"]
+    labels, values = analyzelib.vectorize(text)
+    return jsonify({"labels": labels, "values": values}), 200
--- flaskr/static/script.js
+++ flaskr/static/script.js
@@ -1,3 +1,5 @@
+import { Canvas } from "./components.js";
+
 const jobAnalysisElement = {
   textarea: document.getElementById('jobAnalysisTextarea'),
   button: document.getElementById('jobAnalysisButton'),
@@ -14,4 +16,47 @@ const jsonUploadElement = {
 jobAnalysisElement.textarea.addEventListener('input', (event) => {
   event.currentTarget.style.height = 'auto';
   event.currentTarget.style.height = event.currentTarget.scrollHeight + 'px';
+});
+
+// Perform analysis on the job description given
+jobAnalysisElement.button.addEventListener('click', async () => {
+  // Analyze job description in backend
+  const response = await fetch('/analyze', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      text: jobAnalysisElement.textarea.value
+    })
+  });
+
+  const data = await response.json();
+
+  // Output chart with analysis in frontend
+  const canvas = Canvas('jobAnalysisCanvas');
+  jobAnalysisElement.output.replaceChildren(
+    "Frequently repeated words/phrases used",
+    canvas
+  );
+
+  new Chart(canvas, {
+    type: 'bar',
+    data: {
+      labels: data.labels.slice(0, 10),
+      datasets: [{
+        label: 'Frequency',
+        data: data.values.slice(0, 10),
+        borderwidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
 });
