b38012da97800ff78f9940263f8437d0fbe52628
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-09-10 14:28:30 +0800

    [#19] added support for computing textual similarities between job description and resume summary

---
+++ flaskr/resumelib.py
@@ -0,0 +1,11 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def compute_similarity(corpus):
+    """Computes the TF-IDF similarity of two texts in a given corpus list."""
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    similarity = cosine_similarity(X[0:1], X[1:2]).item()
+    print(similarity)
+    return similarity
