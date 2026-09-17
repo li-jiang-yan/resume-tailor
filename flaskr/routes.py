ac1bec0b41afc5910bcb3b64e9a6df9962ade684
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-09-17 10:17:14 +0800

    [#33] renamed resumelib to similaritylib

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, render_template, request
 
-from . import analyzelib, resumelib
+from . import analyzelib, similaritylib
 
 bp = Blueprint("blog", __name__)
 
@@ -25,5 +25,5 @@ def similarity():
     """Computes the TF-IDF similarity of two texts in a given corpus list."""
     payload = request.get_json()
     corpus = payload["corpus"]
-    similarity = resumelib.compute_similarity(corpus)
+    similarity = similaritylib.compute(corpus)
     return jsonify({"percentage": similarity * 100}), 200
--- flaskr/similaritylib.py
+++ flaskr/similaritylib.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def compute_similarity(corpus):
+def compute(corpus):
     """Computes the TF-IDF similarity of two texts in a given corpus list."""
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(corpus)
