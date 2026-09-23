298ffbc1d689335eb6fac309683250d3282b9ca5
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-09-23 14:06:48 +0800

    [#55] changed arguments of the PrimaryButton component to children instead of text

--- flaskr/static/components/PrimaryButton.js
+++ flaskr/static/components/PrimaryButton.js
@@ -1,12 +1,12 @@
 import { render } from "./render.js";
 
 
-export function PrimaryButton(text) {
-  return render(`
+export function PrimaryButton(...children) {
+  const result = render(`
     <button
       type="button"
-      class="btn btn-primary me-3">
-      ${text}
-    </button>
+      class="btn btn-primary me-3"></button>
   `);
+  result.replaceChildren(...children);
+  return result;
 }
