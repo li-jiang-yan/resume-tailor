da4b38f86855eec9db3888f8a330c28570e8d2dc
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-09-23 18:36:40 +0800

    [#55] removed redundant Accordion component files and put all in one file

--- flaskr/static/components/AccordionBody.js
+++
@@ -1,8 +0,0 @@
-import { render } from "./render.js";
-
-
-export function AccordionBody(...children) {
-  const result = render('<div class="accordion-body"></div>');
-  result.replaceChildren(...children);
-  return result;
-}
--- flaskr/static/components/AccordionItem.js
+++ flaskr/static/components/AccordionItem.js
@@ -1,6 +1,4 @@
 import { render } from "./render.js";
-import { AccordionBody } from "./AccordionBody.js";
-import { AccordionHeader } from "./AccordionHeader.js";
 
 
 export function AccordionItem(buttonText, ...bodyChildren) {
@@ -14,8 +12,33 @@ export function AccordionItem(buttonText, ...bodyChildren) {
 }
 
 
+function AccordionHeader(collapseId, buttonText) {
+  const result = render('<div class="accordion-header"></div>');
+  result.replaceChildren(AccordionButton(collapseId, buttonText));
+  return result;
+}
+
+
+function AccordionButton(collapseId, text) {
+  return render(`
+    <button
+      class="accordion-button"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#${collapseId}">${text}</button>
+  `);
+}
+
+
 function AccordionCollapse(id, ...bodyChildren) {
   const result = render(`<div class="accordion-collapse collapse show" id="${id}"></div>`);
   result.replaceChildren(AccordionBody(...bodyChildren));
+  return result;
+}
+
+
+function AccordionBody(...children) {
+  const result = render('<div class="accordion-body"></div>');
+  result.replaceChildren(...children);
   return result;
 }
